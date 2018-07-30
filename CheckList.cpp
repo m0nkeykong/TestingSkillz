6f7b5ddddc88152f1e337ba93f60304c9323e247
--replace-all <haim1321@gmail.com>
2018-07-30 12:49:36 +0300

    'checklist+textbox'

--- CheckList.cpp
+++ CheckList.cpp
@@ -1,119 +1,72 @@
 #pragma once
 #include "CheckList.h"
 
-//check list Ctor
-CheckList::CheckList(int _height, int _width, vector<string> _options) {
-
-	this->height = _height;
-	this->width = _width;
-	this->options = _options;
+CheckList::CheckList(int height, int width, vector<string> options) {
+	this->height = height;
+	this->width = width;
+	this->options = options;
 	this->checked = { 0, 0, 0 };
 	this->showed = true;
-
 }
 
-//can be focused by project definition
-bool CheckList::canGetFocus() {
-	return true;
-}
-
-
-void CheckList::draw(Graphics g, int i, int j, size_t p) {
+void CheckList::draw(Graphics g, int x, int y, size_t z) {
 	graphics.setBackground(this->background);
 	graphics.setForeground(this->foreground);
 
-	for (int k = 0; k < this->options.size(); k++) {
-		if (this->checked[k] == 0) {
-			graphics.write(i, j + k, "[ ]"); // selection template
-			graphics.write(i + 7, j + k, this->options[k]);	//write options text with X spaces after []
+	for (int i = 0; i < this->options.size(); i++) {
+		if (this->checked[i] == 0) {
+			graphics.write(x, y + i, "( )");
+			graphics.write(x + 7, y + i, this->options[i]);	
 		}
-		//marking selected option with X
-		if (this->checked[k] == 1) {
-			graphics.write(i, j + k, "[X]");
-			graphics.write(i + 7, j + k, this->options[k]);	//write options text with X spaces after []
+		if (this->checked[i] == 1) {
+			graphics.write(x, y + i, "(X)");
+			graphics.write(x + 7, y + i, this->options[i]);	
 		}
-
 	}
 }
 
-//change box status with keyboard - checked/unchecked
-void  CheckList::keyDown(int keyCode, char character) {
-
+//handle key click to change the option in checklist status
+void  CheckList::keyDown(int keyCode, char c) {
 	this->graphics.setCursorVisibility(true);
 
-	for (int i = 0; i < this->options.size(); i++)
-	{
+	for (int i = 0; i < this->options.size(); i++) {
 		graphics.moveTo(this->getLeft() + 1, this->getTop() + i);
-
-
-		int c = 0;
-		c = getchar();
-
+		int _char = getchar();
 
 		do {
-			if (c == VK_TAB) {	//move on to the next control
+			if (_char == VK_TAB)
 				break;
-			}
 			switch (this->checked[i]) {
-			case 1:	//if checked change to unchecked
-				this->checked[i] = 0;
-				putchar(' ');
-				break;
-
-			case 0:	//if unchecked change to checked 
-				this->checked[i] = 1;
-				putchar('X');
-				break;
-
+				case 1:
+					this->checked[i] = 0;
+					putchar(' ');
+					break;
+				case 0:
+					this->checked[i] = 1;
+					putchar('X');
+					break;
 			}
-
 			break;
-		} while (c != VK_TAB);
-
+		} while (_char != VK_TAB);
 	}
-
 }
-//change box status with mouse - checked/unchecked
-void CheckList::mousePressed(int x, int y, DWORD button) {
 
-	for (int i = 0; i < this->getHeight(); i++)
-	{
+//handle mouse click to change the option in checklist status
+void CheckList::mousePressed(int x, int y, DWORD btn) {
+	for (int i = 0; i < this->getHeight(); i++) {
 		if (x == (this->getLeft() + 1) && y == (this->getTop() + i)) {
-
 			graphics.moveTo(this->getLeft() + 1, this->getTop() + i);
 			switch (this->checked[i]) {
-			case 1:	//if checked change to unchecked
-				this->checked[i] = 0;
-				putchar(' ');
-				break;
-
-			case 0:	//if unchecked change to checked 
-				this->checked[i] = 1;
-				putchar('X');
-				break;
-
+				case 1:						//if already checked, unchecked
+					this->checked[i] = 0;
+					putchar(' ');
+					break;
+				case 0:						//if unchecked, make it checked 
+					this->checked[i] = 1;
+					putchar('X');
+					break;
 			}
 			break;
 		}
 	}
 }
-
-//checked boxes
-vector<size_t> CheckList::GetSelectedIndices() {
-
-	return this->checked;
-
-}
-
-//mark box as checekd
-void CheckList::SelectIndex(size_t index) {
-
-	this->checked[index] = 1;
-}
-
-//mark box as unchecked
-void CheckList::DeselectIndex(size_t index) {
-
-	this->checked[index] = 0;
-}
-
